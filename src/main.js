5b5bb5710f845d07ec7f6c16695e68034d870a2f
cara ottmar <cara.ottmar@gmail.com>
2018-05-29 14:34:45 -0700

    scaffold built

---
+++ src/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+require('dotenv').config();
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssPlugin = require(mini-css-extract-plugin);
+
+webpackConfig.entry = `${__dirname}/src/main.js`;
